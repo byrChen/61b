f9954d6643b990fa3e73ec9e5d95b888f2477fc5
byrChen <byrChenXF@gmail.com>
2019-03-18 10:43:48 +0800

    lab1 score

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,8 +1,10 @@
 public class HelloNumbers {
     public static void main(String[] args) {
-        int x = 0;
-        while (x < 10) {
-            System.out.print(x + " ");
+        int x = 1;
+        int sum = 0;
+        while (sum < 46) {
+            System.out.print(sum + " ");
+            sum = x + sum;
             x = x + 1;
         }
     }
